87529a4b5b61a36b6e5c173eff7dcde466bb9b3d
xgqfrms <xgqfrms@outlook.com>
2018-04-25 23:10:18 +0800

    update

---
+++ 000-xyz/ES-Next/template-string.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright
+ * @description
+ * @augments
+ * @example
+ *
+ */
+const  = ( = ``, debug = false) => {
+    // do something...
+    return ;
+};
+
